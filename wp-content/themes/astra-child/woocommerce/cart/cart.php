0eb060b5b4b9d812a6e6b08b33b076d1f0c6754d
anito <axelnitzschner@mac.com>
2022-02-12 12:54:09 +0100

    add coupon code on checkout page if theme option is set

--- wp-content/themes/astra-child/woocommerce/cart/cart.php
+++ wp-content/themes/astra-child/woocommerce/cart/cart.php
@@ -139,7 +139,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 				<td colspan="6" class="actions">
 
 					<?php if ( $wbp_astra_show_coupon_on_cart) { ?>
-						<div class="coupon" style="display: none;">
+						<div class="coupon">
 							<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
 							<?php do_action( 'woocommerce_cart_coupon' ); ?>
 						</div>
