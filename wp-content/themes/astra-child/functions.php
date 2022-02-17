a439f672a82866aacbe87b990a611704b93d2327
anito <axelnitzschner@mac.com>
2022-02-17 23:36:14 +0100

    editor 12

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -280,7 +280,7 @@ function wbp_woo_tab_content($tab_name, $tab)
 
   $title = $product->get_title();
   $content = wpautop($product->get_description()); // prevent to strip out all \n !!!
-  echo '<h3>' . $title . '<br /><strong>Highlights:</strong></h3>' . do_shortcode($content); // keep possible shortcode
+  echo '<h3>' . $title . '<h3><h4>Highlights:</h4>' . do_shortcode($content); // keep possible shortcode
 }
 function wbp_woo_tab_technical()
 {
