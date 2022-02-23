08292e26e40f1907e5a8a7bb8b60eef7beb0564a
anito <axelnitzschner@mac.com>
2022-02-23 23:24:28 +0100

    you can now control whether to sync product tags and tax with product attributes

--- wp-content/themes/astra-child/includes/product_term_handler.php
+++ wp-content/themes/astra-child/includes/product_term_handler.php
@@ -10,22 +10,24 @@ function wbp_process_sale($post_id, $post)
 
   $is_on_sale = $product->is_on_sale();
 
-  // Product Category
+  // Product Sale Category
   $term = get_term_by('name', WC_COMMON_TAXONOMIES['sale'], 'product_cat');
   $term_id = $term->term_id;
   if ($term_id) {
     wbp_set_product_term($product, $term_id, 'cat', $is_on_sale);
   }
 
-  // Product Tag
+  // Product Sale Tag
   $term = get_term_by('name', WC_COMMON_TAXONOMIES['sale'], 'product_tag');
   $term_id = $term->term_id;
   if ($term_id) {
     wbp_set_product_term($product, $term_id, 'tag', $is_on_sale);
   }
 
-  // Product Feature attribute
-  wbp_set_pa_feature_term($product, WC_COMMON_TAXONOMIES['sale'], $is_on_sale);
+  // Product Sale attribute
+  if (SYNC_PRODUCT_ATTS) {
+    wbp_set_pa_term($product, WC_COMMON_TAXONOMIES['sale'], $is_on_sale);
+  }
 }
 
 function wbp_process_featured($product)
@@ -46,13 +48,17 @@ function wbp_process_featured($product)
     wbp_set_product_term($product, $term_id, 'cat', $is_featured);
   }
 
+  // Product Featured tag
   $term = get_term_by('name', WC_COMMON_TAXONOMIES['featured'], 'product_tag');
   if ($term) {
     $term_id = $term->term_id;
     wbp_set_product_term($product, $term_id, 'tag', $is_featured);
   }
 
-  wbp_set_pa_feature_term($product, WC_COMMON_TAXONOMIES['featured'], $is_featured);
+  // Product Featured attribute
+  if(SYNC_PRODUCT_ATTS) {
+    wbp_set_pa_term($product, WC_COMMON_TAXONOMIES['featured'], $is_featured);
+  }
 }
 
 function wbp_set_product_term($product, $term_id, $type, $bool)
@@ -75,7 +81,7 @@ function wbp_set_product_term($product, $term_id, $type, $bool)
 /**
  * Add or remove the "sale" term to/from product attribute "Merkmale"
  */
-function wbp_set_pa_feature_term($product, $term_name, $bool)
+function wbp_set_pa_term($product, $term_name, $bool)
 {
   $name = WC_CUSTOM_PRODUCT_ATTRIBUTES['default'];
   $slug = wc_sanitize_taxonomy_name(stripslashes($name));
