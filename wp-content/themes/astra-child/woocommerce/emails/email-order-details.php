9a00255895cf34bae482e05ee263778af5b46068
anito <axelnitzschner@mac.com>
2022-02-26 09:39:28 +0100

    new i18n astra-child

--- wp-content/themes/astra-child/woocommerce/emails/email-order-details.php
+++ wp-content/themes/astra-child/woocommerce/emails/email-order-details.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Order details table shown in emails.
  *
@@ -15,7 +16,7 @@
  * @version 3.7.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 $text_align = is_rtl() ? 'right' : 'left';
 
@@ -24,19 +25,19 @@ $price_visibility = wbp_get_price_visibility($show_price);
 $price_hidden = !$price_visibility['show'];
 $price_hidden_class = $price_visibility['class'];
 
-do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+do_action('woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email); ?>
 
 <h2>
 	<?php
-	if ( $sent_to_admin ) {
-		$before = '<a class="link" href="' . esc_url( $order->get_edit_order_url() ) . '">';
+	if ($sent_to_admin) {
+		$before = '<a class="link" href="' . esc_url($order->get_edit_order_url()) . '">';
 		$after  = '</a>';
 	} else {
 		$before = '';
 		$after  = '';
 	}
 	/* translators: %s: Order ID. */
-	echo wp_kses_post( $before . sprintf( __( '[Order #%s]', 'woocommerce' ) . $after . ' (<time datetime="%s">%s</time>)', $order->get_order_number(), $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ) );
+	echo wp_kses_post($before . sprintf(__('[Order #%s]', 'woocommerce') . $after . ' (<time datetime="%s">%s</time>)', $order->get_order_number(), $order->get_date_created()->format('c'), wc_format_datetime($order->get_date_created())));
 	?>
 </h2>
 
@@ -44,12 +45,12 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 	<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
 		<thead>
 			<tr>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr($text_align); ?>;"><?php esc_html_e('Product', 'woocommerce'); ?></th>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr($text_align); ?>;"><?php esc_html_e('Quantity', 'woocommerce'); ?></th>
 				<?php
-				if(!$price_hidden) {
+				if (!$price_hidden) {
 				?>
-				<th class="td <?php echo $price_hidden_class ?>" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+					<th class="td <?php echo $price_hidden_class ?>" scope="col" style="text-align:<?php echo esc_attr($text_align); ?>;"><?php esc_html_e('Price', 'woocommerce'); ?></th>
 				<?php
 				}
 				?>
@@ -62,7 +63,7 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 				array(
 					'show_sku'      => $sent_to_admin,
 					'show_image'    => false,
-					'image_size'    => array( 32, 32 ),
+					'image_size'    => array(32, 32),
 					'plain_text'    => $plain_text,
 					'sent_to_admin' => $sent_to_admin,
 				)
@@ -70,31 +71,31 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 			?>
 		</tbody>
 		<?php
-		if(!$price_hidden) {;
+		if (!$price_hidden) {;
 		?>
 			<tfoot class="<?php echo $price_hidden_class ?>">
 				<?php
 				$item_totals = $order->get_order_item_totals();
 
-				if ( $item_totals ) {
+				if ($item_totals) {
 					$i = 0;
-					foreach ( $item_totals as $total ) {
+					foreach ($item_totals as $total) {
 						$i++;
-						?>
+				?>
 						<tr>
-							<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
-							<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
+							<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr($text_align); ?>; <?php echo (1 === $i) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post($total['label']); ?></th>
+							<td class="td" style="text-align:<?php echo esc_attr($text_align); ?>; <?php echo (1 === $i) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post($total['value']); ?></td>
 						</tr>
-						<?php
+					<?php
 					}
 				}
-				if ( $order->get_customer_note() ) {
+				if ($order->get_customer_note()) {
 					?>
 					<tr>
-						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'woocommerce' ); ?></th>
-						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
+						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr($text_align); ?>;"><?php esc_html_e('Note:', 'woocommerce'); ?></th>
+						<td class="td" style="text-align:<?php echo esc_attr($text_align); ?>;"><?php echo wp_kses_post(nl2br(wptexturize($order->get_customer_note()))); ?></td>
 					</tr>
-					<?php
+				<?php
 				}
 				?>
 			</tfoot>
@@ -104,17 +105,17 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 	</table>
 
 	<?php
-	if($sent_to_admin && !empty($price_hidden_class)) {
+	if ($sent_to_admin && !empty($price_hidden_class)) {
 	?>
-	<table class="td" style="margin-top: 8px; font-size: 0.8rem;">
-		<tr>
-			<td class="td <?php echo $price_hidden_class ?>" style="width: 1.5rem;">&nbsp;</td>
-			<td class="td" style="border-width: 0px;">Diese Informationen zum Preis wurden dem Kunden nicht angezeigt. Wende dich bitte an den Admin um diese Einstellung zu ändern.</td>
-		</tr>
-	</table>
+		<table class="td xs" style="margin-top: 8px; border-width: 2px;">
+			<tr>
+				<td class="td <?php echo $price_hidden_class ?>" style="width: 1.5rem;">&nbsp;</td>
+				<td class="nopad" style="border-width: 0px;"><?php echo __('Colorized cells are hidden from the customer. Reach out for an admin in order to change this behaviour.', 'astra-child') ?></td>
+			</tr>
+		</table>
 	<?php
 	}
 	?>
 </div>
 
-<?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+<?php do_action('woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email); ?>
