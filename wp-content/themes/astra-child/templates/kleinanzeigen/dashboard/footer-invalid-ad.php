ec541f9fb08c22c8618d272e9f6303fd26ca4c03
Axel Nitzschner <axelnitzschner@mac.com>
2023-10-25 02:11:45 +0200

    Update footer-invalid-ad.php

--- wp-content/themes/astra-child/templates/kleinanzeigen/dashboard/footer-invalid-ad.php
+++ wp-content/themes/astra-child/templates/kleinanzeigen/dashboard/footer-invalid-ad.php
@@ -1,12 +1,10 @@
 <div style="font-size: 10px;">
   <div style="display: flex; margin-top: -30px;">
-    <span style="flex: 0 0 12%; margin-right: 10px;"><b><?php echo __('Disconnect & hide', 'astra-child') ?>:</b></span>
-    <span style="flex: 0 0 50%;">Anzeige-ID wird vom Produkt entfernt. Produkt wird ausgeblendet.</span>
-    <span style="flex: 0 0 auto;"></span>
+    <span style="flex: 0 0 130px; margin-right: 10px;"><b><?php echo __('Disconnect & hide', 'astra-child') ?>:</b></span>
+    <span style="flex: 0 0 auto;">Anzeige-ID wird vom Produkt entfernt. Produkt wird ausgeblendet.</span>
   </div>
   <div style="display: flex;">
-    <span style="flex: 0 0 12%; margin-right: 10px;"><b><?php echo __('Disconnect', 'astra-child') ?>:</b></span>
-    <span style="flex: 0 0 50%;">Anzeige-ID wird vom Produkt entfernt. Produkt bleibt weiterhin sichtbar.</span>
-    <span style="flex: 0 0 auto;"></span>
+    <span style="flex: 0 0 130; margin-right: 10px;"><b><?php echo __('Disconnect', 'astra-child') ?>:</b></span>
+    <span style="flex: 0 0 auto;">Anzeige-ID wird vom Produkt entfernt. Produkt bleibt weiterhin sichtbar.</span>
   </div>
 </div>
