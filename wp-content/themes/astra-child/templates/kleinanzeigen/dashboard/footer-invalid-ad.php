62c904bf2e19ebfac92e9998ba40f2c4abc86823
Axel Nitzschner <axelnitzschner@mac.com>
2023-11-08 13:23:12 +0100

    updates

--- wp-content/themes/astra-child/templates/kleinanzeigen/dashboard/footer-invalid-ad.php
+++ wp-content/themes/astra-child/templates/kleinanzeigen/dashboard/footer-invalid-ad.php
@@ -1,10 +1,10 @@
 <div style="font-size: 10px;">
   <div style="display: flex; margin-top: -30px;">
     <span style="flex: 0 0 130px; margin-right: 10px;"><b><?php echo __('Hide', 'astra-child') ?>:</b></span>
-    <span style="flex: 0 0 auto;">Produkt wird ausgeblendet. Verknüpfung mit Anzeige wird entfernt.</span>
+    <span style="flex: 0 0 auto;">Verknüpfung mit der Anzeige wird aufgehoben und Produkt wird ausgeblendet.</span>
   </div>
   <div style="display: flex;">
     <span style="flex: 0 0 130px; margin-right: 10px;"><b><?php echo __('Keep visible', 'astra-child') ?>:</b></span>
-    <span style="flex: 0 0 auto;">Produkt bleibt weiterhin sichtbar. Verknüpfung mit Anzeige wird entfernt.</span>
+    <span style="flex: 0 0 auto;">Verknüpfung mit der Anzeige wird aufgehoben aber Produkt bleibt weiterhin sichtbar.</span>
   </div>
 </div>
