b4bdd4c5e72dd674b86947e177448dff9dd414de
Axel Nitzschner <axelnitzschner@mac.com>
2024-01-07 13:43:23 +0100

    v0.2.1

--- wp-content/plugins/kleinanzeigen/admin/partials/kleinanzeigen-admin-display.php
+++ wp-content/plugins/kleinanzeigen/admin/partials/kleinanzeigen-admin-display.php
@@ -1,3 +1,19 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.wplauncher.com
+ * @since      1.0.0
+ *
+ * @package    Kleinanzeigen
+ * @subpackage Kleinanzeigen/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap" id="kleinanzeigen-admin-display">
   <div id="icon-themes" class="icon32"></div>
   <h2><?php echo __('Overview', 'kleinanzeigen') ?></h2>
