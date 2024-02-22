c305ed6e11fe7762ff1a1ce46b18baf4eb6b285b
Axel Nitzschner <axelnitzschner@mac.com>
2024-02-22 01:01:07 +0100

    updated plugins and theme

--- wp-content/plugins/kleinanzeigen/admin/class-kleinanzeigen-admin.php
+++ wp-content/plugins/kleinanzeigen/admin/class-kleinanzeigen-admin.php
@@ -162,7 +162,7 @@ class Kleinanzeigen_Admin extends Kleinanzeigen
   {
     return (self::$schedule = array(
       'never' => array(
-        'interval' => 60,
+        'interval' => 0,
         'display'  => __('Never', 'kleinanzeigen'),
       ),
       'every_minute' => array(
