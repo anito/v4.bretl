91a9194331ec858658faaf83ec181dd5638a54cb
Axel Nitzschner <axelnitzschner@mac.com>
2025-02-11 01:47:45 +0100

    theme and plugin updates

--- wp-content/themes/astra-child/woocommerce/emails/email-header.php
+++ wp-content/themes/astra-child/woocommerce/emails/email-header.php
@@ -10,173 +10,72 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
+ * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 7.4.0
+ * @version 9.6.0
  */
 
-/**
- * changes: hardcoded path to email header logo: $url_logo
- */
-
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
 }
 
-$image = get_option('woocommerce_email_header_image');
-
-// Load colors
-$bg = get_option('woocommerce_email_background_color');
-$body = get_option('woocommerce_email_body_background_color');
-$base = get_option('woocommerce_email_base_color');
-$base_text = wc_light_or_dark($base, '#202020', '#ffffff');
-$text = get_option('woocommerce_email_text_color');
-
-$bg_darker_10 = wc_hex_darker($bg, 10);
-$base_lighter_20 = wc_hex_lighter($base, 20);
-$text_lighter_20 = wc_hex_lighter($text, 20);
-
-// For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline. !important; is a gmail hack to prevent styles being stripped if it doesn't like something.
-$wrapper = "
-    background-color: " . esc_attr($bg) . ";
-    width:100%;
-    -webkit-text-size-adjust:none !important;
-    margin:0;
-    padding: 70px 0 70px 0;
-";
-$template_container = "
-    box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;
-    border-radius:6px !important;
-    background-color: " . esc_attr($body) . ";
-    border: 1px solid $bg_darker_10;
-    border-radius:6px !important;
-";
-$template_header = "
-    background-color: " . esc_attr($base) . ";
-    color: $base_text;
-    border-top-left-radius:6px !important;
-    border-top-right-radius:6px !important;
-    border-bottom: 0;
-    font-family:Arial;
-    font-weight:bold;
-    line-height:100%;
-    vertical-align:middle;
-";
-$body_content = "
-    background-color: " . esc_attr($body) . ";
-    border-radius:6px !important;
-";
-$body_content_inner = "
-    color: $text_lighter_20;
-    font-family:Arial;
-    font-size:14px;
-    line-height:150%;
-    text-align:left;
-";
-$header_content_h1 = "
-    color: " . esc_attr($base_text) . ";
-    margin:0;
-    padding: 28px 24px;
-    text-shadow: 0 1px 0 $base_lighter_20;
-    display:block;
-    font-family:Arial;
-    font-size:30px;
-    font-weight:bold;
-    text-align:left;
-    line-height: 150%;
-";
-$header_image = "
-	width: 300px;"
-;
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset');?>" />
-		<title><?php echo get_bloginfo('name', 'display'); ?></title>
-        <style type="text/css">
-            h2 {
-                font-size: 1.3em !important;
-            }
-            h3 {
-                font-size: 1em !important;
-            }
-            h4 {
-                font-size: 1em !important;
-            }
-            .wc-gzd-item-desc {
-                margin: 0 !important;
-                margin-top: 0.5em !important;
-            }
-            .wc-gzd-item-desc p {
-                font-size: 0.9em !important;
-                line-height: 1em !important;
-                padding-bottom: 5px !important;
-                margin: 0 !important;
-            }
-            .wc-gzd-email-attach-post {
-                font-size: 0.8em !important;
-            }
-            .wc-gzd-email-attach-post h4, .wc-gzd-email-attach-post .wc-gzd-email-attached-content h4 {
-                font-size: 14px !important;
-            }
-            .wc-gzd-email-attach-post {
-                margin: 1em 0 !important;
-            }
-            .wc-gzd-email-attached-content h1, .wc-gzd-email-attached-content h2 {
-                font-size: 15px !important;
-                text-align: left !important;
-                text-shadow: none !important;
-                margin: 0 !important;
-                padding: 0 !important;
-                line-height: 100% !important;
-            }
-            a, a:hover, a:visited, a:focus {
-                color: #333 !important;
-                text-decoration: underline;
-            }
-        </style>
-        </head>
-    	<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
-        <div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>" style="<?php echo $wrapper; ?>" >
-            <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
-                <tr>
-                    <td align="center" valign="top">
-                        <div id="template_header_image">
-                            <?php if ($image): ?>
-                                <p style="margin-top: 0;">
-                                    <a href="<?php echo esc_url(home_url('/')); ?>">
-                                        <img style="<?php echo $header_image; ?>" src="<?php echo $image; ?>" alt="<?php echo get_bloginfo('name', 'display') ?>" />
-                                    </a>
-                                </p>
-                            <?php else: ?>
-                                <a href="<?php echo esc_url(home_url('/')); ?>">
-                                    <h1 style="font-size: 1em; color: #333; text-align: center;"><?php echo bloginfo('name', 'display'); ?></h1>
-                                </a>
-                            <?php endif;?>
-                        </div>
-                            <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container">
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<title><?php echo get_bloginfo( 'name', 'display' ); ?></title>
+	</head>
+	<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+		<table width="100%" id="outer_wrapper">
+			<tr>
+				<td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
+				<td width="600">
+					<div id="wrapper" dir="<?php echo is_rtl() ? 'rtl' : 'ltr'; ?>">
+						<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
 							<tr>
 								<td align="center" valign="top">
-									<!-- Header -->
-									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header">
+									<div id="template_header_image">
+										<?php
+										$img = get_option( 'woocommerce_email_header_image' );
+										/**
+										 * This filter is documented in templates/emails/email-styles.php
+										 *
+										 * @since 9.6.0
+										 */
+										if ( apply_filters( 'woocommerce_is_email_preview', false ) ) {
+											$img_transient = get_transient( 'woocommerce_email_header_image' );
+											$img           = false !== $img_transient ? $img_transient : $img;
+										}
+
+										if ( $img ) {
+											echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" /></p>';
+										}
+										?>
+									</div>
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_container">
 										<tr>
-											<td id="header_wrapper">
-												<h1><?php echo $email_heading; ?></h1>
+											<td align="center" valign="top">
+												<!-- Header -->
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
+													<tr>
+														<td id="header_wrapper">
+															<h1><?php echo esc_html( $email_heading ); ?></h1>
+														</td>
+													</tr>
+												</table>
+												<!-- End Header -->
 											</td>
 										</tr>
-									</table>
-									<!-- End Header -->
-								</td>
-							</tr>
-							<tr>
-								<td align="center" valign="top">
-									<!-- Body -->
-									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
 										<tr>
-											<td valign="top" id="body_content">
-												<!-- Content -->
-												<table border="0" cellpadding="20" cellspacing="0" width="100%">
+											<td align="center" valign="top">
+												<!-- Body -->
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body">
 													<tr>
-														<td valign="top">
-															<div id="body_content_inner">
+														<td valign="top" id="body_content">
+															<!-- Content -->
+															<table border="0" cellpadding="20" cellspacing="0" width="100%">
+																<tr>
+																	<td valign="top">
+																		<div id="body_content_inner">
